6f2e93f62e3227098720eca2b319cf8117381b04
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-05-29 17:21:42 -0600

    Create Leaders in an array.cpp

---
+++ Leaders in an array.cpp
@@ -0,0 +1,26 @@
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        
+        vector<int> ans;
+        int maxi=a[n-1];
+        ans.push_back(maxi);
+        for(int i=n-2;i>=0;i--)
+        {
+            if(a[i]>=maxi)
+            {
+                ans.push_back(a[i]);
+                maxi=a[i];
+            }
+            //maxi = max(maxi,a[i]);
+            
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+        
+        
+        
+    }
+};
